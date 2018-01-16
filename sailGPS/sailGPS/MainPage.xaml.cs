f1de59f2094ae3d4be26e366f7d8240f05ef1c29
David Shin <danwoo0911@gmail.com>
2018-01-16 18:18:36 +0000

    Add project files.

---
+++ sailGPS/sailGPS/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sailGPS
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Padding = new Thickness(10, Device.OnPlatform(20, 5, 5), 10, 10);
+
+		}
+	}
+}
